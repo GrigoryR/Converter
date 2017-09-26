7c1ad357f7299daf3915136833e806c0abd0baad
Gregory Rubinstein <Gregory.E.Rubinstein@gmail.com>
2017-09-25 22:55:59 -0400

    Add files via upload

---
+++ Converter.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConverterLibrary
+{
+    public class Converter
+    {
+        public const decimal LBS_TO_KGS = 1 / 2.20462M;
+        public const decimal INCH_TO_CM = 2.54M;
+        public const int INCH_TO_FT = 12;
+
+        public enum Category
+        {
+            Length = 1,
+            Weight,
+            Temperature,
+            Area,
+            Volume
+        }
+
+        public enum MesurementSystem
+        {
+            Metric = 1,
+            British,
+            Scientific
+        }
+
+        public MesurementSystem From { get; set; }
+        public MesurementSystem To { get; set; }
+
+        public decimal GetBMI(decimal metric_height, decimal metric_weight)
+        {
+            decimal bmi = 0;
+            try
+            {
+                bmi = metric_weight / (metric_height * metric_height);
+            }
+            catch (Exception ex)
+            {
+                // Exception handling logic goes here
+                //ex.Message;
+            }
+            return bmi;
+        }
+
+        public decimal LengthToMetric(int feet, decimal inches)
+        {
+            decimal resulting_length = 0;
+            try
+            {
+                decimal inches_in_feet = feet * INCH_TO_FT;
+                resulting_length = (inches + inches_in_feet) * INCH_TO_CM;
+            }
+            catch (Exception ex)
+            {
+                // ex.Message;
+            }
+            return resulting_length;
+        }
+
+        public decimal WeightToMetric(decimal lbs)
+        {
+            decimal resulting_weight = lbs * LBS_TO_KGS;
+            return resulting_weight;
+        }
+
+        public Converter()
+        {
+
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ConverterLibrary;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGet_Click(object sender, RoutedEventArgs e)
+        {
+            Converter c = new Converter();
+            decimal meters;
+            decimal.TryParse(txtHeight.Text, out meters);
+            decimal kilograms;
+            decimal.TryParse(txtWeight.Text, out kilograms);
+            lblResult.Content = "BMI: " + c.GetBMI(meters,kilograms).ToString();
+        }
+
+        private bool IsValidInput(string txt)
+        {
+            bool IsValid = System.Text.RegularExpressions.Regex.IsMatch(txt, @"\d{0,3}");
+            return IsValid;
+        }
+
+        private void ToggleButton(bool IsValid, TextBox tbx)
+        {
+            if (IsValid)
+            {
+                tbx.Background = Brushes.White;
+                btnGet.IsEnabled = true;
+            }
+            else
+            {
+                tbx.Background = Brushes.Yellow;
+                btnGet.IsEnabled = false;
+            }
+        }
+
+        private void txtWeight_LostFocus(object sender, RoutedEventArgs e)
+        {
+            bool IsValid = IsValidInput(txtWeight.Text);
+            ToggleButton(IsValid, txtWeight);
+        }
+
+        private void txtHeight_LostFocus(object sender, RoutedEventArgs e)
+        {
+            bool IsValid = IsValidInput(txtHeight.Text);
+            ToggleButton(IsValid, txtHeight);
+        }
+    }
+}
